9a0683e0e7e79df1aeeec2482380d8bdcedf8b23
stanley-kim <stanley.will.kim@gmail.com>
2023-05-21 22:59:05 +0900

    Audio programming with p5: playback controls

--- Week2/sketch.js
+++ Week2/sketch.js
@@ -1,8 +1,13 @@
 let mySound;
 let playStopButton;
+let jumpButton;
+let sliderVolume;
+let sliderRate;
+let sliderPan;
 
 function preload() {
-    mySound = loadSound('./sounds/AfterLIKE.mp3');    
+    soundFormats('mp3');
+    mySound = loadSound('./sounds/AfterLIKE');    
 }
 
 function setup() {
@@ -11,9 +16,21 @@ function setup() {
     background(180);
     
     playStopButton = createButton('play');
-    playStopButton.position(20, 20);
+    playStopButton.position(200, 20);
     playStopButton.mousePressed(playStopSound);
+    jumpButton = createButton('jump');
+    jumpButton.position(250, 20);
+    jumpButton.mousePressed(jumpSound);
     
+    sliderVolume = createSlider(0, 2, 1, 0.01);
+    sliderVolume.position(20, 20);
+    text('volume', 80, 20);
+    sliderRate = createSlider(-2, 2, 1, 0.01);
+    sliderRate.position(20, 70);
+    text('rate', 80, 65);
+    sliderPan = createSlider(-1, 1, 0, 0.01);
+    sliderPan.position(20, 115);
+    text('pan', 80, 110);
 }
 
 function playStopSound() {
@@ -30,6 +47,16 @@ function playStopSound() {
     console.log(getAudioContext().state);
 }
 
+function jumpSound() {
+    let dur = mySound.duration();
+    let t = random(dur);
+    mySound.jump(t);
+    console.log(t);
+}
+
 function draw() {
   // put drawing code here
+    mySound.setVolume(sliderVolume.value());
+    mySound.rate(sliderRate.value());
+    mySound.pan(sliderPan.value());
 }
