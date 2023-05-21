ac6ef99219a4eac7f36999b9bee4c6e5b6126eb9
stanley-kim <stanley.will.kim@gmail.com>
2023-05-21 16:49:27 +0900

    loading and playing back audio files

--- Week2/sketch.js
+++ Week2/sketch.js
@@ -1,5 +1,33 @@
+let mySound;
+let playStopButton;
+
+function preload() {
+    mySound = loadSound('./sounds/AfterLIKE.mp3');    
+}
+
 function setup() {
   // put setup code here
+    createCanvas(400, 400);
+    background(180);
+    
+    playStopButton = createButton('play');
+    playStopButton.position(20, 20);
+    playStopButton.mousePressed(playStopSound);
+    
+}
+
+function playStopSound() {
+    if(mySound.isPlaying()) {
+        mySound.stop();
+        //mySound.pause();
+        playStopButton.html('play');
+    }
+    else {
+        //mySound.play();
+        mySound.loop();
+        playStopButton.html('stop');        
+    }
+    console.log(getAudioContext().state);
 }
 
 function draw() {
