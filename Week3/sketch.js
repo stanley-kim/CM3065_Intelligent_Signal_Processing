1d9cd42129c00e5779f31d4cfcf0dd6f0a944086
stanley-kim <stanley.will.kim@gmail.com>
2023-05-24 22:33:09 +0900

    Add files via upload

--- Week3/sketch.js
+++ Week3/sketch.js
@@ -1,7 +1,20 @@
+let player;
+
+function preload() {
+    player = loadSound("../sounds/AfterLIKE.mp3");
+}
+
 function setup() {
   // put setup code here
+    var canv = createCanvas(200, 200);
+    background(0, 255, 0);
+    canv.mousePressed(mousePressed);
 }
 
 function draw() {
   // put drawing code here
+}
+
+function mousePressed() {
+    player.loop();
 }
